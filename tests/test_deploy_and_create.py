5afe0d71726452e66ed0633628924990e7dfd215
Nibbles <me@nibbledbit.co.uk>
2022-01-21 20:55:13 +0000

    building

--- tests/test_deploy_and_create.py
+++ tests/test_deploy_and_create.py
@@ -1,7 +1,9 @@
-from brownie import network, PrettyFreakinPlainPFP, config
+from brownie import network, PrettyFreakinPlainPFP, config, exceptions
+from scripts.deploy import deploy_pfp_nft
 from scripts.helpful_scripts import (
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
-    get_account,
+    get_publish_account,
+    get_personal_account,
     get_contract,
     fund_with_link,
     deploy_mocks,
@@ -15,36 +17,40 @@ def test_can_create():
     random_number = 777
     requestId = creation_tx.events["requestedCollectible"]["requestId"]
     get_contract("vrf_coordinator").callBackWithRandomness(
-        requestId, random_number, pfp_nft, {"from": get_account()}
+        requestId, random_number, pfp_nft, {"from": get_publish_account()}
     )
-    assert pfp_nft.mintCalls() > 0
-    assert pfp_nft.randomCalls() > 0
+    assert pfp_nft.timesMinted() > 0
+
+
+def test_duplicate_nft():
+    personal_account = get_personal_account()
+    print(f"Account {personal_account}")
+
+    pfp_nft = deploy_pfp_nft()
+
+    fund_with_link(pfp_nft.address)
+
+    tx = pfp_nft.publicMint(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {"from": personal_account})
+    tx.wait(1)
+
+    fund_with_link(pfp_nft.address)
+    with pytest.raises(exceptions.VirtualMachineError):
+        tx = pfp_nft.publicMint(
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {"from": personal_account}
+        )
+        tx.wait(1)
 
 
 def deploy_pfp_and_create_nft():
     # Get Account
-    print("Getting account")
-    account = get_account()
-    print(f"Account {account}")
+    personal_account = get_personal_account()
+    print(f"Account {personal_account}")
 
-    # configure dependencies
-    print(f"The active network is {network.show_active()}")
-
-    print("Deploying PrettyFreakinPlainPFP")
-    pfp_nft = PrettyFreakinPlainPFP.deploy(
-        get_contract("vrf_coordinator"),
-        get_contract("link_token"),
-        config["networks"][network.show_active()]["keyhash"],
-        config["networks"][network.show_active()]["fee"],
-        {"from": account},
-        publish_source=config["networks"][network.show_active()].get("verify"),
-    )
-    print(f"Contract deployed to {pfp_nft}")
-    print(f"Token Counter: {pfp_nft.tokenCounter()}")
+    pfp_nft = deploy_pfp_nft()
 
     fund_with_link(pfp_nft.address)
 
-    tx = pfp_nft.publicMint(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {"from": account})
+    tx = pfp_nft.publicMint(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {"from": personal_account})
     tx.wait(1)
 
     return pfp_nft, tx
